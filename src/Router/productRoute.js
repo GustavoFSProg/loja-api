b69a00598d1e9aabb24ed054f8b81faeb1c9496d
Gustavo <gustavosohne38@gmail.com>
2020-12-21 09:05:52 -0300

    endnd

--- src/Router/productRoute.js
+++ src/Router/productRoute.js
@@ -10,7 +10,7 @@ const route = new Router()
 
 route.get('/', productsController.getAll)
 route.delete('/delete/:id', productsController.apagar)
-route.get('/:id', productsController.getById)
+route.get('/find/:id', productsController.getById)
 route.post('/product', upload.single('image'), productsController.create)
 route.put('/update/:id', productsController.update)
 
